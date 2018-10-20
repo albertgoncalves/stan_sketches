b67bc74eda9e5e4918718f3d2bc52a15c172e56d
albertgoncalves <goncalves.albert@gmail.com>
2018-10-20 19:31:11 -0400

    More.

--- mark.py
+++ mark.py
@@ -16,14 +16,14 @@ def label_captures(pop, sample_sizes):
 
 
 def main():
-    pop          = 750 # population to be estimated from generated data
-    subpop       = 20  # "true" sample event rate
-    n_samples    = 10  # number of capture events
-    sample_sizes = sample_counts(subpop, n_samples)  # sample event sizes
-    labels       = label_captures(pop, sample_sizes) # sampled pop labels
-    label_counts = Counter(labels) # {label: recapture frequency}
-    freq         = list(label_counts.values()) # don't need labels, just freq!
-    min_bound    = len(label_counts.keys()) # number of unique labels
+    pop          = 750  # population to be estimated from generated data
+    subpop       = 20   # "true" sample event rate
+    n_samples    = 10   # number of capture events
+    sample_sizes = sample_counts(subpop, n_samples)   # sample event sizes
+    labels       = label_captures(pop, sample_sizes)  # sampled pop labels
+    label_counts = Counter(labels)  # {label: recapture frequency}
+    freq         = list(label_counts.values())  # don't need labels, just freq!
+    min_bound    = len(label_counts.keys())  # number of unique labels
     data         = { 'n_samples'   : n_samples
                    , 'sample_sizes': sample_sizes
                    , 'n'           : len(freq)
